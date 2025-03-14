4886e54e202f37925384ff1aa58dd40af81c9c6c
alpkycn <alperen.kayacn@gmail.com>
2025-03-14 03:49:50 +0100

    Update add_blog.php
    a small change

--- mvc_project/views/admin/add_blog.php
+++ mvc_project/views/admin/add_blog.php
@@ -1,7 +1,7 @@
 <div class="container">
     <h1>Add New Blog Post</h1>
     <form method="POST" action="/admin/addBlog">
-        <label>Titel</label>
+        <label>Title</label>
         <input type="text" name="title" required>
         <label>Contents</label>
         <textarea name="content" required></textarea>
